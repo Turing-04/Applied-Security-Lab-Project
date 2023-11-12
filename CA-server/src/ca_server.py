f013f3f973df49494682a69d859ec4c9b552fe30
Niels Lachat <niels.lachat@epfl.ch>
2023-11-12 17:38:28 +0100

    start impl request-certificate

--- CA-server/src/ca_server.py
+++ CA-server/src/ca_server.py
@@ -1,7 +1,45 @@
-from flask import Flask
+import subprocess
+from flask import Flask, request
+import re
 
 app = Flask(__name__)
 
+OPENSSL_CMD = "openssl"
+OPENSSL_KEY_PARAMS = "rsa:2048"
+TMP_PRIV_KEY_PATH = "tmp.key"
+TMP_CSR_PATH = "tmp.csr"
+
+CERT_ORG_NAME = "iMovies"
+
+def build_subj_str(user_info: Dict[str, str]) -> str:
+    out = f"/C=CH/O={CERT_ORG_NAME}"
+
+    firstname = user_info["firstname"]
+    lastname = user_info["lastname"]
+    assert firstname.isalpha(), firstname
+    assert lastname.isalpha(), lastname
+    out += f"/CN={firstname} {lastname}"
+
+    email_regex = r"\w+@\w+\.\w+"
+    email = user_info["email"]
+    assert bool(re.match(email_regex, email)), email
+    out += f"emailAddress={email}/"
+    
+    return out
+
+def make_csr(user_info: Dict[str, str]) -> None:
+    """
+    openssl req -new \
+        -newkey rsa:2048 -nodes -keyout tmp.key \
+        -out tmp.csr \
+        -subj "/C=CH/O=iMovies/CN=Lukas Bruegger/emailAddress=lb@imovies.ch/"
+    """
+    cmd = [OPENSSL_CMD]
+    cmd += ["req", "-new"]
+    cmd += ["-newkey", OPENSSL_KEY_PARAMS, "-nodes", "-keyout", TMP_PRIV_KEY_PATH]
+    cmd += ["-out", TMP_CSR_PATH]
+    cmd += ["-subj", subj_str]
+    subprocess.run(cmd) 
 
 @app.post("/request-certificate")
 def request_certificate():
@@ -35,6 +73,14 @@ def request_certificate():
     The CA server also sends the freshly generated certificate + key pair 
     to the backup server, encrypted with the master backup public key.
     """
+    user_info = request.get_json()
+    for key, val in user_info.items():
+        assert key in ["uid", "lastname", "firstname", "email"]
+        assert type(val) == str
+
+
+    make_csr(user_info)
+
 
     # TODO don't forget to send cert.p12 encrypted to the backup server
     # TODO don't forget to delete the private key and cert.p12 file
