4c76edb9a8ba73343193be4a88595be6f21f892a
Lin Zhang <linzhang@student-net-lee-3362.intern.ethz.ch>
2023-11-16 12:47:32 +0100

    iterate mysql.sh

--- mysql_server/scripts/setup_mysql.sh
+++ mysql_server/scripts/setup_mysql.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-apt update
-apt upgrade -y
+# export SYNCED_FOLDER="/vagrant"
+
+sudo apt update
+sudo apt upgrade -y
 
 # MySQL dows not work properly on debian bookworm
 # install mysql
@@ -14,11 +16,23 @@ apt upgrade -y
 
 # 1. Install MariaDB: https://linuxgenie.net/how-to-install-mariadb-on-debian-12-bookworm-distribution/
 # Notes: remove remote connection to root, only localhost, add the root password during the isntallation process.
-apt install mariadb-server -y
-mariadb-secure-installation
+sudo apt install mariadb-server -y
+mysql_secure_installation <<EOF
+
+n
+y
+root
+root
+y
+y
+y
+y
+EOF
 
 # 2.Login into mysql: https://www.digitalocean.com/community/tutorials/how-to-import-and-export-databases-in-mysql-or-mariadb#step-2-mdash-importing-a-mysql-or-mariadb-database
-mysql -u root -p
+mysql -u root -proot -e "CREATE DATABASE imovies;"
+
+
 # To add the password for the user (out of installation process), example for root: 
 # ALTER USER 'root'@'localhost' IDENTIFIED BY 'root'
 # flush privileges;
@@ -26,37 +40,33 @@ mysql -u root -p
 
 # 3. Create database and load data
 # Think about the schema to remove vulneratibilities
-CREATE DATABASE imovies;
+
 # Exit mysql and copy imovies.db to tmp folder (using Shared folder feature in Virtual Box) and import it in MariaDB
-mysql -u root -p imovies <  /tmp/imovies.db
+mysql -u root -proot imovies < $SYNCED_FOLDER/imovies_users.db
 
 # How often to do the backup? What happends on the password change (if no backup)? SHhoud system administrator be in charge?
 
 # 4. Create certificates table
-mysql -u root -p imovies 
+mysql -u root -proot imovies -e "CREATE TABLE certificates (uid varchar(64) NOT NULL, certificate varchar(6000) NOT NULL, expire_timestamp timestamp  NOT NULL, revoke_timestamp timestamp);"
+
+
 # we checked the types, etc of tables users, to confirm:
 # SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH FROM INFORMATION_SCHEMA.Columns where table_name="users";
 # update column datatype by:
 # ALTER table MODIFY COLUMN column_name desired_datatype;
 
-CREATE TABLE certificate (
-    uid varchar(64) NOT NULL,
-    certificate varchar(256) NOT NULL,   
-    expire_timestamp timestamp  NOT NULL,
-    revoke_timestamp timestamp
-);
 
 # 5. Create users in MySQL and set privileges
 # two users: webserver with read/write access to table "users" and read access to table "certificate"; and caserver with read/write access to the "certificates" table.
 # the above two users are remote users, for security TODO update ip address by replacing '%' in the following code
-CREATE USER 'webserver'@'%' IDENTIFIED BY 'webserver123';
-GRANT SELECT, INSERT, UPDATE, DELETE ON users TO 'webserver'@'%';
-GRANT SELECT ON certificates TO 'webserver'@'%';
+mysql -u root -proot imovies -e "CREATE USER 'webserver'@'%' IDENTIFIED BY 'webserver123';"
+mysql -u root -proot imovies -e "GRANT SELECT, INSERT, UPDATE, DELETE ON users TO 'webserver'@'%';"
+mysql -u root -proot imovies  -e "GRANT SELECT ON certificates TO 'webserver'@'%';"
 
-CREAT USERS, 'caserver'@'%' IDENTIFIED BY 'caserver123';
-GRANT SELEECT, INSERT, UPDATE, DELETE ON certificates TO 'caserver'@'%';
+mysql -u root -proot imovies -e "CREAT USERS, 'caserver'@'%' IDENTIFIED BY 'caserver123';"
+mysql -u root -proot imovies -e "GRANT SELEECT, INSERT, UPDATE, DELETE ON certificates TO 'caserver'@'%';"
 
-FLUSH PRIVILEGES;
+mysql -u root -proot imovies -e "FLUSH PRIVILEGES;"
 
 # configure mysql to accept remote connections:
 # edit the MySQL configuration file in /etc/mysql/. 
@@ -70,10 +80,10 @@ FLUSH PRIVILEGES;
 
 # 6. [TO CHECK ON THE MACHINE] Create sysadmin user and add it to the sudoers group
 # 6.1. create user and set the password:
-# sudo adduser <username>
+sudo adduser sysadmin
 
 # 6.2. add user to the sudo group
-# sudo usermod -aG sudo <username>
+sudo usermod -aG sudo sysadmin
 
 # 6.3. to verify two options:
 # getent group sudo 
