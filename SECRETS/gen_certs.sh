4161eb6bd138f73f60bab6f61cc31541b307102f
Niels Lachat <niels.lachat@epfl.ch>
2023-11-20 12:23:02 +0100

    Update HTTPS certs + gen backup-master-key
    I updated the certificates for the HTTPS certificates, so that the
    Common Name is the IP of the server. I think this allows clients to
    verify the authenticity of the server better.
    
    I also generated a backup-master-key to allow encrypting pkcs12
    certs+private keys sent to the backup server

--- SECRETS/gen_certs.sh
+++ SECRETS/gen_certs.sh
@@ -1,41 +1,69 @@
 #!/bin/bash
 
-# This script generates all the static (i.e. not for regular employees) 
-# certificates and private keys for the machines and users of the system
+# Function to generate a new certificate and key for a given entity
+generate_certificate() {
+    entity="$1"
+    cacert_path="$2"
+    cakey_path="$3"
+    ca_password_path="$4"
+    common_name="$5"
+    tmp_csr=$(mktemp)
+    
+    mkdir -p "$entity"
+    openssl req -new \
+        -newkey rsa:2048 -nodes -keyout "$entity/$entity.key" \
+        -out $tmp_csr \
+        -subj "/C=CH/ST=Zurich/O=iMovies/CN=$common_name/emailAddress=$entity@imovies.ch/"
 
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 /path/to/ca_password.txt"
-    exit 1
-fi
+    sudo openssl x509 -req -in $tmp_csr -out "$entity/$entity.crt" \
+        -CA $cacert_path -CAkey $cakey_path -passin file:$ca_password_path -CAcreateserial -days 365
 
-ca_password_path=$1
+    # display generated cert
+    openssl x509 -in "$entity/$entity.crt" -text
 
-if [ ! -e "$ca_password_path" ]; then
-    echo "The ca password file $ca_password_path does not exist"
+    # verify cert
+    openssl verify -CAfile $cacert_path "$entity/$entity.crt"
+}
+
+USAGE="Usage: $0 [--regen-all | --gen-new <new_cert_name> <common_name>] /path/to/ca_password.txt"
+
+# Main script
+if [ "$#" -lt 1 ]; then
+    echo $USAGE
     exit 1
 fi
 
 cakey_path="./ca-server/cakey.pem"
 cacert_path="./ca-server/cacert.pem"
-# First, we genereate the ca cert and key
-sudo openssl req -passout file:"$1" -new -x509 \
-    -extensions v3_ca -keyout $cakey_path -out $cacert_path -days 3650 \
-    -subj "/C=CH/ST=Zurich/O=iMovies/CN=iMovies root cert/emailAddress=ca-admin@imovies.ch/"
 
 
-# we generate the private keys and signed certificates for the entities which need it
-
-entities=("mysql-server" "webserver-intranet" "webserver-https"\
-    "backup-server" "ca-server-https" "ca-server-intranet" "ca-admin" "sysadmin-ssh")
-for entity in "${entities[@]}"; do
-    tmp_csr=$(mktemp)
-
-    mkdir -p "$entity"
-    openssl req -new \
-        -newkey rsa:2048 -nodes -keyout "$entity/$entity.key" \
-        -out $tmp_csr \
-        -subj "/C=CH/ST=Zurich/O=iMovies/CN=$entity/emailAddress=$entity@imovies.ch/"
-
-    sudo openssl x509 -req -in $tmp_csr -out "$entity/$entity.crt" \
-        -CA $cacert_path -CAkey $cakey_path -passin file:$ca_password_path -CAcreateserial -days 365
-done
+case "$1" in
+    --regen-all)
+        ca_password_path=$2
+        # Regenerate all certificates, including the CA cert
+        sudo rm -rf "./ca-server" && mkdir "./ca-server"
+        sudo openssl req -passout file:"$ca_password_path" -new -x509 \
+            -extensions v3_ca -keyout $cakey_path -out $cacert_path -days 3650 \
+            -subj "/C=CH/ST=Zurich/O=iMovies/CN=iMovies root cert/emailAddress=ca-admin@imovies.ch/"
+        entities=("mysql-server" "webserver-intranet" "backup-master-key"
+            "backup-server" "ca-server-intranet" "ca-admin" "sysadmin-ssh")
+        for entity in "${entities[@]}"; do
+            generate_certificate $entity $cacert_path $cakey_path $ca_password_path $entity
+        done
+        ;;
+    --gen-new)
+        # Generate a new directory with a signed certificate and key for the specified name
+        if [ "$#" -ne 4 ]; then
+            echo $USAGE
+            exit 1
+        fi
+        entity="$2"
+        common_name="$3"
+        ca_password_path="$4"
+        generate_certificate $entity $cacert_path $cakey_path $ca_password_path $common_name
+        ;;
+    *)
+        echo $USAGE
+        exit 1
+        ;;
+esac
