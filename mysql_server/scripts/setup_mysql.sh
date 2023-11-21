13de865e5e8f9c50321952b7f34f8ae0fc8f2d35
kamifai <kamifai14@gmail.com>
2023-11-21 03:39:44 +0100

    Client authentication, describe problem

--- mysql_server/scripts/setup_mysql.sh
+++ mysql_server/scripts/setup_mysql.sh
@@ -31,23 +31,19 @@ mysql -u root -proot imovies -e "CREATE TABLE certificates (uid varchar(64) NOT 
 # 5. Create users in MySQL and set privileges
 # webserver with read/write access to table "users" and read access to table "certificate";
 # caserver with read/write access to the "certificates" table.
-# [TODO] update ip address by replacing '%' in the following code
-mysql -u root -proot imovies -e "CREATE USER 'webserver'@'%' IDENTIFIED BY '}DqG3mZ8neKPp?#Uc?49K&W2';"
+# [TODO] update ip address of web server
+mysql -u root -proot imovies -e "CREATE USER 'webserver'@'%' IDENTIFIED BY '}DqG3mZ8neKPp?#Uc?49K&W2' REQUIRE X509;"
 mysql -u root -proot imovies -e "GRANT SELECT, INSERT, UPDATE, DELETE ON users TO 'webserver'@'%';"
 mysql -u root -proot imovies -e "GRANT SELECT ON certificates TO 'webserver'@'%';"
 
-mysql -u root -proot imovies -e "CREATE USER 'caserver'@'%' IDENTIFIED BY 'cn9@1kbka;}=(iPgEMO1&{XW';"
-mysql -u root -proot imovies -e "GRANT SELECT, INSERT, UPDATE, DELETE ON certificates TO 'caserver'@'%';"
+mysql -u root -proot imovies -e "CREATE USER 'caserver'@'10.0.0.3' IDENTIFIED BY 'cn9@1kbka;}=(iPgEMO1&{XW' REQUIRE X509;"
+mysql -u root -proot imovies -e "GRANT SELECT, INSERT, UPDATE, DELETE ON certificates TO 'caserver'@'10.0.0.3';"
 
 mysql -u root -proot -e "FLUSH PRIVILEGES;"
-# validate the user privileges
-# SELECT User, host, db, table_name, table_priv from mysql.tables_priv;
-
 # 6. Change bind-address from localhost to the interface in the configuration file.
 sudo sed -i "s/.*bind-address.*/bind-address = 10.0.0.5/" /etc/mysql/mariadb.conf.d/50-server.cnf
 
 # 7. Enable TLS in mariadb
-# [TODO] openSSL and SSL are both enabled, check if okay
 # 7.1 Create folders for certs and keys
 mkdir /etc/mysql/ssl
 mkdir /etc/mysql/ssl/certs
@@ -60,12 +56,23 @@ sudo chmod 644 /etc/mysql/ssl/certs/mysql-server-crt.pem /etc/mysql/ssl/certs/ca
 
 cp $SYNCED_FOLDER/mysql-server-key.pem /etc/mysql/ssl/private
 sudo chmod 640 /etc/mysql/ssl/private/mysql-server-key.pem
-sudo chgrp mysql /etc/mysql/ssl/private/mysql-server-key.pem
+sudo chown root:mysql /etc/mysql/ssl/private/mysql-server-key.pem
 
 # 7.3 Set TLS configuration in MariaDB
 cp $SYNCED_FOLDER/mariadb-server-tls.cnf /etc/mysql/mariadb.conf.d
 sudo systemctl restart mariadb
 
+# 7.4 [TODO on WEBSERVER and CASERVER] Set Client certificate authentication on the client machine
+# To enable client authentication, parameters below should be set in the clients:
+
+#ssl_cert = /etc/my.cnf.d/certificates/client-cert.pem
+#ssl_key = /etc/my.cnf.d/certificates/client-key.pem
+#ssl_ca = /etc/my.cnf.d/certificates/ca.pem
+
+# 7.5 Enable server authentication om the client side
+# This parameter on the lcient side gives an error. Soemthing is wrong with the server authenctication.
+
+#ssl-verify-server-cert
 # 8 Create sysadmin user and add it to the sudoers group
 sudo useradd -m sysadmin -p dv8RCJruycKGyN
 sudo usermod -aG sudo sysadmin
