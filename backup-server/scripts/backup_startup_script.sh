2135f8d5e3d20c7b83f82ee7773056c047ce02a7
Lin Zhang <linzhang@Aleks-MacBook-Pro.local>
2023-11-21 16:05:47 +0100

    backup script init

--- backup-server/scripts/backup_startup_script.sh
+++ backup-server/scripts/backup_startup_script.sh
@@ -16,5 +16,20 @@ sudo apt install duplicity -y
 cp $SYNCED_FOLDER/SECRETS/backup-server/backup-server.key /home/.ssh/backup-server.key
 cp $SYNCED_FOLDER/SECRETS/backup-server/backup-server.pgp /home/.gnupg/duplicity.pgp
 
+# TODO copy public key to client machines for backing up
 
+# prepare backup directory
+mkdir -p /srv/duplicity/webserver /srv/duplicity/caserver /srv/duplicity/mysql
+
+# run backup setup script
+echo "start backup server cron setup"
+# create dedicated backup scripts directory
+mkdir -p /etc/duplicity/scripts
+# copy backup setup script to above dir
+cp $SYNCED_FOLDER/scripts/backup_setup.sh /etc/duplicity/scripts/backup_setup.sh
+chmod +x /etc/duplicity/scripts/backup_setup.sh
+
+# run backup cron job setup
+bash "$SYNCED_FOLDER/scripts/cron_setup_backup.sh"
+echo "done backup server cron setup"
 
