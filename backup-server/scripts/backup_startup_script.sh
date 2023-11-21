bca4b0231586085b244e4cd4f08732273a1dbc33
lz <mail.linzhang@gmail.com>
2023-11-22 00:36:54 +0100

    created unprivileged backupusr

--- backup-server/scripts/backup_startup_script.sh
+++ backup-server/scripts/backup_startup_script.sh
@@ -5,31 +5,41 @@ export VAGRANT_HOME="/home/vagrant"
 
 echo "Startup script started"
 
-echo "Starting install of necessary software"
-# allows to get newer packages than the box creation (e.g. python3.11-venv)
+echo "Starting to install necessary software"
+# system updadte
 sudo apt update
 
 # install duplicity
 sudo apt install duplicity -y 
 
-# TODO copy backup private key for ssh connection to ~/.ssh
-cp $SYNCED_FOLDER/SECRETS/backup-server/backup-server.key /home/.ssh/backup-server.key
-cp $SYNCED_FOLDER/SECRETS/backup-server/backup-server.pgp /home/.gnupg/duplicity.pgp
+# create backup user and add backup to sudoers group
+sudo useradd -m backupusr -p go41QYHlCEIvoOlc54
 
-# TODO copy public key to client machines for backing up
+# create /home/backup directory and set permissions
+mkdir -p /home/backup/.ssh /home/backup/.gnupg
+sudo chown -R backupusr:backupusr /home/backup
+
+# TODO copy backup private key for ssh connection to /.ssh
+# cp $SYNCED_FOLDER/SECRETS/backup-server/backup-server.key /home/backup/.ssh/backup-server.key
+# cp $SYNCED_FOLDER/SECRETS/backup-server/backup-server.pgp /home/backup/.gnupg/duplicity.pgp
+mkdir -p /home/backup/.ssh && cp $SYNCED_FOLDER/SECRETS/authorized_keys /home/root/.ssh/authorized_keys
+sudo chmod -R go=/home/backup/.ssh
+sudo chmod -R go-rwx /home/backup/.gnupg
 
 # prepare backup directory
 mkdir -p /srv/duplicity/webserver /srv/duplicity/caserver /srv/duplicity/mysql
+sudo chown -R backupusr:backupusr /srv/duplicity
 
-# run backup setup script
-echo "start backup server cron setup"
-# create dedicated backup scripts directory
-mkdir -p /etc/duplicity/scripts
-# copy backup setup script to above dir
-cp $SYNCED_FOLDER/scripts/backup_setup.sh /etc/duplicity/scripts/backup_setup.sh
-chmod +x /etc/duplicity/scripts/backup_setup.sh
+# set up client agent backup - server cron job is no longer needed
+# # run backup setup script
+# echo "start backup server cron setup"
+# # create dedicated backup scripts directory
+# mkdir -p /etc/duplicity/scripts
+# # copy backup setup script to above dir
+# cp $SYNCED_FOLDER/scripts/backup_setup.sh /etc/duplicity/scripts/backup_setup.sh
+# chmod +x /etc/duplicity/scripts/backup_setup.sh
 
-# run backup cron job setup
-bash "$SYNCED_FOLDER/scripts/cron_setup_backup.sh"
-echo "done backup server cron setup"
+# # run backup cron job setup
+# bash "$SYNCED_FOLDER/scripts/cron_setup_backup.sh"
+# echo "done backup server cron setup"
 
