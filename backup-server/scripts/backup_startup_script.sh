2425a50870cbd9e3d0d89d1f37c5a68968862dba
lz <mail.linzhang@gmail.com>
2023-11-22 18:49:41 +0100

    redo users on backup

--- backup-server/scripts/backup_startup_script.sh
+++ backup-server/scripts/backup_startup_script.sh
@@ -12,19 +12,48 @@ sudo apt update
 # install duplicity
 sudo apt install duplicity -y 
 
-# create backup user and add backup to sudoers group
-sudo useradd -m backupusr -p go41QYHlCEIvoOlc54
+# Step 1: create users and automatically their home directory
+#--------------------------------------------
+echo "creating users and home directories..."
+# create backup user 
+sudo useradd -m backupusr -p TmikweJoB7tVpobBcT
+mkdir -p /home/backupusr/.ssh
+sudo chmod 700 /home/backupusr/.ssh
 
-# create /home/backup directory and set permissions
-mkdir -p /home/backup/.ssh /home/backup/.gnupg
-sudo chown -R backupusr:backupusr /home/backup
+# create caserver user
+sudo useradd -m caserver -p TUsZNJZR4Nlx9Du1nN
+mkdir -p /home/caserver/.ssh && sudo chmod 700 /home/caserver/.ssh
+touch /home/caserver/.ssh/authorized_keys && sudo chmod 600 /home/caserver/.ssh/authorized_keys
 
-# TODO copy backup private key for ssh connection to /.ssh
-# cp $SYNCED_FOLDER/SECRETS/backup-server/backup-server.key /home/backup/.ssh/backup-server.key
-# cp $SYNCED_FOLDER/SECRETS/backup-server/backup-server.pgp /home/backup/.gnupg/duplicity.pgp
-mkdir -p /home/backup/.ssh && cp $SYNCED_FOLDER/SECRETS/authorized_keys /home/root/.ssh/authorized_keys
-sudo chmod -R go=/home/backup/.ssh
-sudo chmod -R go-rwx /home/backup/.gnupg
+# create webserver user
+sudo useradd -m webserver -p dFP9s2ohTsCSXBHTmt
+mkdir -p /home/webserver/.ssh && sudo chmod 700 /home/webserver/.ssh
+touch /home/webserver/.ssh/authorized_keys && sudo chmod 600 /home/webserver/.ssh/authorized_keys
+
+# create mysql user
+sudo useradd -m mysql -p bUDvwzw5cVaETMBrIo
+mkdir -p /home/mysql/.ssh && sudo chmod 700 /home/mysql/.ssh
+touch /home/mysql/.ssh/authorized_keys && sudo chmod 600 /home/mysql/.ssh/authorized_keys
+
+# create sysadmin user and add sysadmin to sudo group
+sudo useradd -m sysadmin -p RbNoH9BGxO1FcyTXc1
+sudo usermod -aG sudo sysadmin
+mkdir -p /home/sysadmin/.ssh && sudo chmod 700 /home/sysadmin/.ssh
+touch /home/sysadmin/.ssh/authorized_keys && sudo chmod 600 /home/sysadmin/.ssh/authorized_keys
+
+# Step 2: copy public keys to authorized_keys files
+#--------------------------------------------
+# caserver
+
+# webserver
+
+# mysql
+
+# sysadmin
+
+# Step 3: configure sshd for uni-directional ssh connection: only from clients to backup server
+#--------------------------------------------
+
 
 # prepare backup directory
 mkdir -p /srv/duplicity/webserver /srv/duplicity/caserver /srv/duplicity/mysql
@@ -43,3 +72,4 @@ sudo chown -R backupusr:backupusr /srv/duplicity
 # bash "$SYNCED_FOLDER/scripts/cron_setup_backup.sh"
 # echo "done backup server cron setup"
 
+# set up sshd with unidirectional key authentication
