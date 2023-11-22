92f52432f8e0ebec57a9cab57956a6a4fab0cb36
Niels Lachat <niels.lachat@epfl.ch>
2023-11-22 15:00:26 +0100

    Remove useless ssh-authorized-keys

--- SECRETS/gen_ssh.sh
+++ SECRETS/gen_ssh.sh
@@ -12,18 +12,3 @@ for ((i=0; i<${#entities[@]}; i++)); do
     passphrase="${passphrases[$i]}"
     ssh-keygen -t rsa -N "$passphrase" -f "$dst_priv_key" -C "$comment"
 done
-
-mkdir -p ssh-authorized-keys
-
-# Machines only the sysadmin can ssh into:
-only_sysadmin_can_ssh=("webserver" "ca-server" "mysql-server")
-for target_machine in "${only_sysadmin_can_ssh[@]}"; do
-    cp ./sysadmin-ssh/sysadmin-ssh.pub "./ssh-authorized-keys/${target_machine}_authorized_keys"
-done
-
-# Authorized keys for backup-server
-bkp_server_authorized_keys="./ssh-authorized-keys/backup-server_authorized_keys"
-echo > "$bkp_server_authorized_keys"
-for entity in "${entities[@]}"; do
-    cat "./$entity/$entity.pub" >> "$bkp_server_authorized_keys"
-done
