94299215e1dc5e79c6a75bcd24bf83293a7b7f04
Lin Zhang <linzhang@student-net-lee-3362.intern.ethz.ch>
2023-11-23 11:50:59 +0100

    updated keys

---
+++ SECRETS/gen_x509.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+CAKEY_PATH="./ca-server/cakey.pem"
+CACERT_PATH="./ca-server/cacert.pem"
+
+# Function to generate a new certificate and key for a given entity
+generate_certificate() {
+    entity="$1"
+    ca_password_path="$2"
+    common_name="$3"
+    tmp_csr=$(mktemp)
+    
+    mkdir -p "$entity"
+    openssl req -new \
+        -newkey rsa:2048 -nodes -keyout "$entity/$entity.key" \
+        -out $tmp_csr \
+        -subj "/C=CH/ST=Zurich/O=iMovies/CN=$common_name/emailAddress=$entity@imovies.ch/"
+
+    sudo openssl x509 -req -in $tmp_csr -out "$entity/$entity.crt" \
+        -CA $CACERT_PATH -CAkey $CAKEY_PATH -passin file:$ca_password_path -CAcreateserial -days 365
+
+    # display generated cert
+    # openssl x509 -in "$entity/$entity.crt" -text
+
+    # # verify cert
+    # openssl verify -CAfile $cacert_path "$entity/$entity.crt"
+}
+
+USAGE="Usage: $0 [--regen-all | --gen-new <new_cert_name> <common_name>] /path/to/ca_password.txt"
+
+# Main script
+if [ "$#" -lt 1 ]; then
+    echo $USAGE
+    exit 1
+fi
+
+
+
+
+case "$1" in
+    --regen-all)
+        ca_password_path="$2"
+        if [ -z "$ca_password_path" ]; then
+            echo $USAGE
+            exit 1
+        fi
+        # Regenerate all certificates, including the CA cert
+        mkdir -p "./ca-server"
+        sudo openssl req -passout file:"$ca_password_path" -new -x509 \
+            -extensions v3_ca -keyout $CAKEY_PATH -out $CACERT_PATH -days 3650 \
+            -subj "/C=CH/ST=Zurich/O=iMovies/CN=iMovies root cert/emailAddress=ca-admin@imovies.ch/"
+
+        entities=("webserver-mysql" "webserver-https" "ca-server-https" "ca-server-mysql" "mysql-server" "ca-admin")
+        common_names=("10.0.1.2" "1.2.3.4" "10.0.0.3" "10.0.0.3" "10.0.0.5" "ca-admin")
+
+        for ((i=0; i<${#entities[@]}; i++)); do
+            entity="${entities[$i]}"
+            common_name="${common_names[$i]}"
+            generate_certificate "$entity" "$ca_password_path" "$common_name"
+        done
+        ;;
+    --gen-new)
+        # Generate a new directory with a signed certificate and key for the specified name
+        if [ "$#" -ne 4 ]; then
+            echo $USAGE
+            exit 1
+        fi
+        entity="$2"
+        common_name="$3"
+        ca_password_path="$4"
+        generate_certificate $entity $ca_password_path $common_name
+        ;;
+    *)
+        echo $USAGE
+        exit 1
+        ;;
+esac
