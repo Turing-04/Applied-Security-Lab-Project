ed5d8dc35997b20c9271a3886af5a48cc4b19b65
Turing-04 <marin.cornuot@gmail.com>
2023-11-24 23:26:12 +0100

    added setup for apache2 and more

--- webserver/scripts/setup_flask.sh
+++ webserver/scripts/setup_flask.sh
@@ -12,8 +12,10 @@ source .venv/bin/activate
 python3 -m pip install -r requirements.txt
 
 # start the flask app
-python3 app.py
+# python3 app.py
 # Should probably be run with gunicorn or something similar
 
 # migt also need to generate secret session key for the flask app
 export SECRET_KEY=$(python3 -c 'import os; print(os.urandom(16))')
+
+echo "Python virtual environment created and flask app ready"
--- webserver/scripts/startup_script.sh
+++ webserver/scripts/startup_script.sh
@@ -16,8 +16,10 @@
 
 #!/bin/bash
 
+export SYNCED_FOLDER="/vagrant"
 export VAGRANT_HOME="/home/vagrant"
-WEBSERVER_DIR="/var/www/webserver"
+WEBSERVER_ROOT="/var/www/webserver"
+WEBSERVER_PASSWORD="webserver"
 
 
 echo "Starting script for webserver setup"
@@ -27,7 +29,8 @@ echo "Starting script for webserver setup"
 # need to handle user creation and rights
 # probably need to create a user with sudo rights and then delete the vagrant user
 sudo useradd webserver --create-home --shell /bin/bash
-sudo usermod -aG sudo webserver # should it be sudo or www-data ?
+sudo usermod -aG sudo webserver # should it really have sudo rights ?
+echo "webserver:$WEBSERVER_PASSWORD" | sudo chpasswd
 
 sudo apt update -y && sudo apt upgrade -y
 sudo apt install -y python3 python3-pip python3-venv python3-dev
@@ -36,15 +39,64 @@ sudo apt install -y apache2 apache2-dev
 sudo apt install -y openssl libssl-dev
 sudo apt install -y ssh
 
-# setup Apache2 - TODO
+# TODO: setup SSH
+#############################################
+# TODO: setup sysadmin user
+#############################################
+
+
+# setup Apache2 
+sudo a2enmode wsgi
 sudo a2enmod ssl
-sudo a2enmod proxy
-# sudo a2enmod wsgi
+
+echo "copy keys for certificate"
+
 
 # need to handle the keys here
 
 # setup the flask webserver and call flask_setup.sh ? 
 # TODO
+cp "$SYNCED_FOLDER/SECRETS/webserver-https/webserver-https.crt" /etc/ssl/certs/webserver-https.crt
+cp "$SYNCED_FOLDER/SECRETS/webserver-https/webserver-https.key" /etc/ssl/private/webserver-https.key
+chown webserver /etc/ssl/certs/webserver-https.crt
+chmod u=r,go= /etc/ssl/certs/webserver-https.crt
+
+echo "Copy apache2 config file"
+cp "$SYNCED_FOLDER/config/webserver.conf" /etc/apache2/sites-available/
+cp "$SYNCED_FOLDER/config/apache2.conf" /etc/apache2/apache2.conf
+cp "$SYNCED_FOLDER/config/envvars" /etc/apache2/envvars
+
+
+# config mysql client
+echo "Copy webserver-mysql crt and key"
+cp "$SYNCED_FOLDER/SECRETS/webserver-mysql/webserver-mysql.crt" /etc/ssl/certs/webserver-mysql.crt
+cp "$SYNCED_FOLDER/SECRETS/webserver-mysql/webserver-mysql.key" /etc/ssl/private/webserver-mysql.key
+chown webserver /etc/ssl/certs/webserver-mysql.crt
+chmod u=r,go= /etc/ssl/certs/webserver-mysql.crt
+# set permissions for private key
+chown --recursive webserver /etc/ssl/private
+chmod u+x /etc/ssl/private
+chmod u=r,go= /etc/ssl/private/webserver-mysql.key
+
+# setup flask webserver
+echo "Copy src to $WEBSERVER_ROOT"
+mkdir -p "$WEBSERVER_ROOT"
+cp -r "$SYNCED_FOLDER/src/" "$WEBSERVER_ROOT/"
+rm -r "$WEBSERVER_ROOT/src/__pycache__" "$WEBSERVER_ROOT/src/.venv"
+cp "$SYNCED_FOLDER/scripts/setup_flask.sh" "$WEBSERVER_ROOT"
+
+sudo chown --recursive webserver "$WEBSERVER_ROOT"
+
+sudo chmod u+x "$WEBSERVER_ROOT/setup_flask.sh"
+sudo -u webserver "$WEBSERVER_ROOT/setup_flask.sh"
+
+# start apache2
+sudo a2ensite webserver
+sudo apache2ctl configtest
+sudo systemctl restart apache2
+
+# test the webserver
+wget --no-check-certificate https://localhost:443/
 
 
 # start webserver 
