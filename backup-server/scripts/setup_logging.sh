40fec3fd645db6442065a27feb576bc14e046de3
kamifai <kamifai14@gmail.com>
2023-11-25 21:29:47 +0100

    Logging: save logs in a correct path

--- backup-server/scripts/setup_logging.sh
+++ backup-server/scripts/setup_logging.sh
@@ -12,9 +12,10 @@ sudo cp $SYNCED_FOLDER/SECRETS/logging-rsyslog/logging-rsyslog.crt /etc/syslog-n
 sudo cp $SYNCED_FOLDER/SECRETS/ca-server/cacert.pem /etc/syslog-ng/ssl/certs
 sudo chmod 644 /etc/syslog-ng/ssl/certs/logging-rsyslog.crt /etc/syslog-ng/ssl/certs/cacert.pem
 
-cp $SYNCED_FOLDER/SECRETS/logging-rsyslog/logging-rsyslog.key /etc/syslog-ng/ssl/private
+sudo cp $SYNCED_FOLDER/SECRETS/logging-rsyslog/logging-rsyslog.key /etc/syslog-ng/ssl/private
 sudo chmod 600 /etc/syslog-ng/ssl/private/logging-rsyslog.key
 
 # 3. Replace rsyslog configuration file
 sudo cp $SYNCED_FOLDER/syslog-ng.conf /etc/syslog-ng
+sudo cat $SYNCED_FOLDER/hosts >> /etc/hosts
 sudo systemctl restart syslog-ng
