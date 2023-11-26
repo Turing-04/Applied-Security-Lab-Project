e75aa6a40a9ed500e4dfc2dfdf85b93e59a90612
lz <mail.linzhang@gmail.com>
2023-11-26 10:43:45 +0100

    update script

--- mysql_server/scripts/setup_backup.sh
+++ mysql_server/scripts/setup_backup.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-# install duplicity
-sudo apt install duplicity -y
+# create backup storage in backupusr home directory and set permission
+mkdir /home/backupusr/mysql
+chown backupusr:backupusr /home/backupusr/mysql
+chmod 700 /home/backupusr/mysql
 
-# backup with duplicity
+# create database logical backup dump
+mysqldump --database imovies -uroot -pH6Mue92MeNNnvFRpJ67V > /home/backupusr/mysql/imovies-$(date '+%Y-%m-%d').sql
+chown backupusr:backupusr /home/backupusr/mysql/imovies-$(date '+%Y-%m-%d').sql
+chmod 600 /home/backupusr/mysql/imovies-$(date '+%Y-%m-%d').sql
+
+
+# backup with sftp
 su -u backupusr
 
 duplicity /etc/mysql/ sftp://mysql@10.0.0.4//backup/mysql/mariadb/
