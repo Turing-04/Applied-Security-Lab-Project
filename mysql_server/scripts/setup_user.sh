bb80d08d02c03b1d3f0bac201047f0ef834916ad
Lin Zhang <linzhang@Aleks-MacBook-Pro.local>
2023-11-27 00:15:59 +0100

    update user passwords

--- mysql_server/scripts/setup_user.sh
+++ mysql_server/scripts/setup_user.sh
@@ -1,8 +1,10 @@
 # SYSADMIN
-sudo useradd -m sysadmin -p dv8RCJruycKGyN
+sudo useradd -m sysadmin
+echo 'sysadmin:dv8RCJruycKGyN' | sudo chpasswd
 sudo usermod -aG sudo sysadmin
 sudo chmod 700 /home/sysadmin
 
 # BACKUP
-sudo useradd -m backupusr -p vzO107Z4icPL15VhmB
+sudo useradd -m backupusr
+echo 'backupusr:vzO107Z4icPL15VhmB' | sudo chpasswd
 sudo chmod 700 /home/backupusr
