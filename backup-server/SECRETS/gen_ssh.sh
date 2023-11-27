8f1f753a475341c7727c27c70d565ebf432091fa
Lin Zhang <linzhang@Aleks-MacBook-Pro.local>
2023-11-27 20:06:19 +0100

    update backup-server SECRETS

--- backup-server/SECRETS/gen_ssh.sh
+++ backup-server/SECRETS/gen_ssh.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-entities=("ca-server-ssh" "mysql-server-ssh" "sysadmin-ssh")
-passphrases=("" "" "flail-dandelion-concierge")
+entities=("firewall-ssh" "webserver-ssh" "ca-server-ssh" "mysql-server-ssh" "sysadmin-ssh")
+passphrases=("" "" "" "" "flail-dandelion-concierge")
 
 for ((i=0; i<${#entities[@]}; i++)); do
     entity="${entities[$i]}"
