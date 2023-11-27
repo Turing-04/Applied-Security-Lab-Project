a7962377e42ee6a83e3944f1d17f55d40939dd0f
lz <mail.linzhang@gmail.com>
2023-11-27 21:45:30 +0100

    revert change

--- mysql_server/scripts/backup_mysql.sh
+++ mysql_server/scripts/backup_mysql.sh
@@ -12,11 +12,13 @@ chmod 600 /home/backupusr/mysql/imovies-$(date '+%Y-%m-%d').sql
 # variables
 REMOTE_USER='mysql'
 REMOTE_HOST='10.0.0.4'
+HOST_NAME='backupuser'
 REMOTE_DIR='/backup/mysql/mariadb'
 LOCAL_FILE_PATH=/home/backupusr/mysql/imovies-$(date '+%Y-%m-%d').sql
 
 # sftp command
-sftp $REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR <<EOF
+# sftp $REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR <<EOF
+sftp $HOST_NAME:$REMOTE_DIR <<EOF
 cd $REMOTE_DIR
 put $LOCAL_FILE_PATH
 EOF
