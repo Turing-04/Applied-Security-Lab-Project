bacc7f5f8701fb8c61171cd81a4c5122a8a22b57
Lin Zhang <linzhang@Aleks-MacBook-Pro.local>
2023-11-27 23:09:24 +0100

    finally script works, TODO hostkeychecking

--- mysql_server/scripts/backup_mysql.sh
+++ mysql_server/scripts/backup_mysql.sh
@@ -1,26 +1,38 @@
 #!/bin/bash
 
 # create database logical backup dump
+echo "starting to create mysql logical backup >>>"
 mysqldump --databases imovies -uroot -pH6Mue92MeNNnvFRpJ67V > /home/backupusr/mysql/imovies-$(date '+%Y-%m-%d').sql
 chown backupusr:backupusr /home/backupusr/mysql/imovies-$(date '+%Y-%m-%d').sql
 chmod 600 /home/backupusr/mysql/imovies-$(date '+%Y-%m-%d').sql
+echo "finishing creating mysql logical backup..."
 
 # switch user
-su backupusr
-
-# variables
-REMOTE_USER='mysql'
-REMOTE_HOST='10.0.0.4'
-HOST_NAME='backupserver'
-REMOTE_DIR='/backup/mysql/mariadb'
-LOCAL_FILE_PATH=/home/backupusr/mysql/imovies-$(date '+%Y-%m-%d').sql
-
+# echo "change backupuser profile again???"
+su backupusr<<REALEND
 # sftp command
-# sftp $REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR <<EOF
-sftp $HOST_NAME:$REMOTE_DIR <<EOF
-cd $REMOTE_DIR
-put $LOCAL_FILE_PATH
+echo "starting sftp >>>"
+sftp backupserver:/backup/mysql/mariadb <<EOF
+cd /backup/mysql/mariadb
+put /home/backupusr/mysql/imovies-$(date '+%Y-%m-%d').sql
 EOF
+echo "finishing sftp file transfer..."
+REALEND
 
+echo "first exit"
 exit
-exit
+
+
+# # variables
+# echo "setting up variables >>>"
+# REMOTE_USER='mysql'
+# REMOTE_HOST='10.0.0.4'
+# HOST_NAME='backupserver'
+# REMOTE_DIR='/backup/mysql/mariadb'
+# LOCAL_FILE_PATH=/home/backupusr/mysql/imovies-$(date '+%Y-%m-%d').sql
+# echo "finishing setting up variables..."
+## sftp how to use
+# sftp $HOST_NAME:$REMOTE_DIR <<EOF
+# cd $REMOTE_DIR
+# put #LOCAL_FILE_PATH
+# EOFÍ
