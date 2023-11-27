e78790e308d92f00834bee59631870079acd661a
lz <mail.linzhang@gmail.com>
2023-11-27 21:37:21 +0100

    rollback sftp cmd

--- mysql_server/scripts/backup_mysql.sh
+++ mysql_server/scripts/backup_mysql.sh
@@ -10,12 +10,13 @@ chmod 600 /home/backupusr/mysql/imovies-$(date '+%Y-%m-%d').sql
 
 
 # variables
-HOST_NAME='backupserver'
+REMOTE_USER='mysql'
+REMOTE_HOST='10.0.0.4'
 REMOTE_DIR='/backup/mysql/mariadb'
 LOCAL_FILE_PATH=/home/backupusr/mysql/imovies-$(date '+%Y-%m-%d').sql
 
 # sftp command
-sftp $HOST_NAME:$REMOTE_DIR <<EOF
+sftp $REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR <<EOF
 cd $REMOTE_DIR
 put $LOCAL_FILE_PATH
 EOF
