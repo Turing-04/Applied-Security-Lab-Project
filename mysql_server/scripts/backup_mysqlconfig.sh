0511991db5c476d22b63f8af0ca27eafb4bfdb9c
lz <mail.linzhang@gmail.com>
2023-11-28 12:09:30 +0100

    cron job for backing up the config files on mysql server

---
+++ mysql_server/scripts/backup_mysqlconfig.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# switch user
+su backupusr<<REALEND
+# sftp command
+echo "starting sftp >>>"
+sftp -o StrictHostKeyChecking=no backupserver:/backup/mysql/config <<EOF
+cd /backup/mysql/config
+put /etc/ssh/sshd_config /etc/ssh/sshd_config.d /etc/mysql/mariadb.cnf /etc/mysql/mariadb.conf.d /etc/ssl/openssl.cnf
+mv /backup/mysql/config/sshd_config /backup/mysql/config/sshd_config-$(date '+%Y-%m-%d')
+mv /backup/mysql/config/sshd_config.d /backup/mysql/config/sshd_config-$(date '+%Y-%m-%d').d
+mv /backup/mysql/config/mariadb.cnf /backup/mysql/config/mariadb-$(date '+%Y-%m-%d').cnf
+mv /backup/mysql/config/mariadb.conf.d /backup/mysql/config/mariadb-$(date '+%Y-%m-%d').conf.d
+mv /backup/mysql/config/openssl.cnf /backup/mysql/config/openssl-$(date '+%Y-%m-%d').cnf
+EOF
+echo "finishing sftp file transfer..."
+REALEND
+
+exit
