faab8f5fe53b04b19cf6722ab8071aeb1ac802a5
Lin Zhang <linzhang@student-net-hg-dock-1-c-0190.intern.ethz.ch>
2023-11-29 14:46:30 +0100

    clean code: rm comments

--- mysql_server/scripts/backup_mysql.sh
+++ mysql_server/scripts/backup_mysql.sh
@@ -22,20 +22,3 @@ echo "finishing sftp file transfer..."
 REALEND
 
 exit
-
-
-
-# # variables
-# echo "setting up variables >>>"
-# REMOTE_USER='mysql'
-# REMOTE_HOST='10.0.0.4'
-# HOST_NAME='backupserver'
-# REMOTE_DIR='/backup/mysql/mariadb'
-# LOCAL_FILE_PATH=/home/backupusr/mysql/imovies-$(date '+%Y-%m-%d').sql
-# echo "finishing setting up variables..."
-## sftp how to use
-# sftp $HOST_NAME:$REMOTE_DIR <<EOF
-# cd $REMOTE_DIR
-# put #LOCAL_FILE_PATH
-# EOFÍ
-# rm /backup/mysql/mariadb/imovies-$(date -d 'yesterday' '+%Y-%m-%d').sql
