60114c119ddaed6a094f59b2d5aa93266389a099
Turing-04 <116177492+Turing-04@users.noreply.github.com>
2023-11-29 11:52:47 +0100

    prevent privilege esclation attack using cronjob run by root

--- mysql_server/scripts/setup_cron.sh
+++ mysql_server/scripts/setup_cron.sh
@@ -4,8 +4,8 @@
 #--------------------------------------------
 # copy mysql backup script
 cp $SYNCED_FOLDER/scripts/backup_mysql.sh /home/backupusr/scripts/backup_mysql.sh
-chown backupusr:backupusr /home/backupusr/scripts/backup_mysql.sh
-chmod 700 /home/backupusr/scripts/backup_mysql.sh
+chown root:root /home/backupusr/scripts/backup_mysql.sh
+chmod 500 /home/backupusr/scripts/backup_mysql.sh
 
 # backup mysql script
 SCRIPT="/home/backupusr/scripts/backup_mysql.sh"
@@ -20,8 +20,8 @@ SCHEDULE="45 22 * * *"
 #--------------------------------------------
 # copy mysql server config backup script
 cp $SYNCED_FOLDER/scripts/backup_mysqlconfig.sh /home/backupusr/scripts/backup_mysqlconfig.sh
-chown backupusr:backupusr /home/backupusr/scripts/backup_mysqlconfig.sh
-chmod 700 /home/backupusr/scripts/backup_mysqlconfig.sh
+chown root:root /home/backupusr/scripts/backup_mysqlconfig.sh
+chmod 500 /home/backupusr/scripts/backup_mysqlconfig.sh
 
 # add backup mysql config files cron job
 (crontab -l 2>/dev/null; echo "40 16 * * * /home/backupusr/scripts/backup_mysqlconfig.sh") | crontab -
